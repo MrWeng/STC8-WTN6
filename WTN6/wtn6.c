42b4d43965f9508388373bb34caeb6a7d47608a8
MrWeng <panjiangweng@foxmail.com>
2020-08-13 23:20:17 +0800

    1、新增WTN6系列语音芯片资料 2、更新语音内容以及更新字段地址 3、更新报数字时，当数字个位为0时，报0时的错误，直接播报十 4、当数字个位为0时，不播报数字个位的0 5、修改延时时间，让语音连贯性更自然

--- WTN6/wtn6.c
+++ WTN6/wtn6.c
@@ -115,7 +115,6 @@ void WTN6_Num(unsigned char Num[],int Len)
 		{	
 			Delay_ms(500);			//延时1秒
 			WTN6_Line_1(Num[i]);
-			Delay_ms(500);
 		}
 }
 /*
@@ -126,33 +125,64 @@ void WTN6_Num(unsigned char Num[],int Len)
  *@author		MrWeng
  *@date			2020-8-7
  */
-void WTN6_Temp(unsigned char Num[])
+void WTN6_Temp(unsigned char Temp[])
 {
 	WTN6_Temp_Type temp;						//温度
-	WTN6_Symbol_Type Celsius,Dot;			   //摄氏度,点
+	WTN6_Symbol_Type Deg,Dot;			    	//度,点
+	WTN6_Num_Unit_CN_Type Ten;				    //十
 	
 	temp 	=  WTN6_TH_Temp;	
-	Celsius =  WTN6_S_TC;	
+	Deg     =  WTN6_S_Deg;	
 	Dot		=  WTN6_S_D;
+	Ten		=  WTN6_NUC_TE;
 	
 	//格式：温度 23.5度
-	Delay_ms(1000);					
-	WTN6_Line_1(temp);							//播报温度
+	Delay_ms(500);					
+	WTN6_Line_1(temp);								//播报温度
+	Delay_ms(500);		
 	
-	Delay_ms(1500);					
-	WTN6_Line_1(Num[0]);						//温度十位
+	switch(Temp[0])
+	{
+		case 0x00:										//如果十位为0，从个位开始报
+			Delay_ms(500);					
+			WTN6_Line_1(Temp[1]);						//温度个位
+		break;
+		case 0x01:										//如果十位为1，播十和十个位
+			Delay_ms(500);					
+			WTN6_Line_1(Ten);					       	//播报十
+			if(Temp[1]!=0x00)
+			{
+				Delay_ms(500);					
+				WTN6_Line_1(Temp[1]);						//温度个位
+			}
+		break;
+		default:										//如果十位不为0、1
+			Delay_ms(500);					
+			WTN6_Line_1(Temp[0]);						//温度十位
+		
+			Delay_ms(500);					
+			WTN6_Line_1(Ten);					       	//播报十
+			if(Temp[1]!=0x00)
+			{
+				Delay_ms(500);					
+				WTN6_Line_1(Temp[1]);						//温度个位
+			}
+	}
 	
-	Delay_ms(1000);					
-	WTN6_Line_1(Num[1]);						//温度个位
+//	if(Temp[0] == 0x00)
+//	{
+//		Delay_ms(500);					
+//		WTN6_Line_1(Temp[1]);						//温度个位
+//	}else if(Temp[0])
 	
-	Delay_ms(1000);					
-	WTN6_Line_1(Dot);							//播报点
+	Delay_ms(500);					
+	WTN6_Line_1(Dot);								//播报点
 	
-	Delay_ms(1000);			
-	WTN6_Line_1(Num[2]);						//温度小数位
+	Delay_ms(500);				
+	WTN6_Line_1(Temp[2]);							//温度小数位
 	
-	Delay_ms(1000);			
-	WTN6_Line_1(Celsius);						//播报度
+	Delay_ms(500);			
+	WTN6_Line_1(Deg);						    	//播报度
 }
 
 /*
@@ -163,33 +193,61 @@ void WTN6_Temp(unsigned char Num[])
  *@author		MrWeng
  *@date			2020-8-7
  */
-void WTN6_Hump(unsigned char Num[])
+void WTN6_Hump(unsigned char Hump[])
 {
 	WTN6_Temp_Type hum;							//湿度
 	WTN6_Symbol_Type Per_Cent,Dot;				//百分之,点
-	
+	WTN6_Num_Unit_CN_Type Ten;				    //十
 	hum	 	 = WTN6_TH_Hum;
 	Dot		 = WTN6_S_D;
 	Per_Cent = WTN6_S_TH;
+	Ten		 = WTN6_NUC_TE;
 	
 	//格式：温度 23.5%
-	Delay_ms(1000);					
+	Delay_ms(500);					
 	WTN6_Line_1(hum);							//播报湿度
 	
-	Delay_ms(1500);			
+	Delay_ms(500);			
 	WTN6_Line_1(Per_Cent);						//播报百分之
+	Delay_ms(500);
 	
-	Delay_ms(1500);					
-	WTN6_Line_1(Num[0]);						//温度十位
 	
-	Delay_ms(1000);					
-	WTN6_Line_1(Num[1]);						//温度个位
+	switch(Hump[0])
+	{
+		case 0x00:										//如果十位为0，从个位开始报
+			Delay_ms(500);					
+			WTN6_Line_1(Hump[1]);						//湿度个位
+		break;
+		case 0x01:										//如果十位为1，播十和十个位
+			Delay_ms(500);					
+			WTN6_Line_1(Ten);					       	//播报十
+			
+			if(Hump[1]!=0x00)
+			{
+				Delay_ms(500);					
+				WTN6_Line_1(Hump[1]);						//湿度个位
+			}
+		break;
+		default:										//如果十位不为0、1
+			Delay_ms(500);					
+			WTN6_Line_1(Hump[0]);						//湿度十位
+		
+			Delay_ms(500);					
+			WTN6_Line_1(Ten);					       	//播报十
+			
+			if(Hump[1]!=0x00)
+			{
+				Delay_ms(500);					
+				WTN6_Line_1(Hump[1]);						//湿度个位
+			}
+	}
 	
-	Delay_ms(1000);					
+
+	Delay_ms(500);					
 	WTN6_Line_1(Dot);							//播报点
 	
-	Delay_ms(1000);			
-	WTN6_Line_1(Num[2]);						//温度小数位	
+	Delay_ms(500);			
+	WTN6_Line_1(Hump[2]);						//湿度小数位	
 }
 
 /*
@@ -202,9 +260,9 @@ void WTN6_Ring(void)
 {
 	WTN6_Phone_Type ring;
 	ring = WTN6_P_ICM;
-	Delay_ms(1000);			
+	Delay_ms(500);			
 	WTN6_Line_1(ring);						
-	Delay_ms(1000);				
+	Delay_ms(500);				
 }	
 
 /*
@@ -220,12 +278,12 @@ void WTN6_InCall(void)
 	ring = WTN6_P_IC;
 	Delay_ms(500);			
 	WTN6_Line_1(ring);						
-	Delay_ms(1000);				
+	Delay_ms(500);				
 }	
 
 /*
   *@brief 		播报日期
- *				播报内容为“今天XXXX年X月X日”
+ *				播报内容为“今天是XXXX年X月X日”
  *@param[in]	Date：需要播报的日期
  *				例如：2020年8月18日，Date[]={0x02,0x00,0x02,0x00,0x00,0x08,0x01,0x08};
  *@author		MrWeng
@@ -233,78 +291,97 @@ void WTN6_InCall(void)
  */
 void WTN6_ReportDate(unsigned char Date[])						
 {
-//	unsigned char Year_Num[4],Month_Num[2],Day_Num[2];
 	int i;
 	
 	WTN6_Date_Type Today,Year,Month,Day;
+	WTN6_Symbol_Type Is;
+	WTN6_Num_Unit_CN_Type Ten;				    //十
+	
 	Today = WTN6_D_TD;
 	Year  = WTN6_D_Y;
 	Month = WTN6_D_M;
 	Day   = WTN6_D_D;
+	Is    = WTN6_S_Is;
+	Ten	  = WTN6_NUC_TE;
 	
-	Delay_ms(1000);			
-	WTN6_Line_1(Today);
-	Delay_ms(1000);
+	Delay_ms(500);			
+	WTN6_Line_1(Today);				//播报今天
 	
-//播报年	
+	Delay_ms(500);
+	WTN6_Line_1(Is);				//播报是
+	
+//循环年的数字
 	for(i=0;i<4;i++)
 	{	
 		Delay_ms(500);			
-		WTN6_Line_1(Date[i]);
-		Delay_ms(500);
+		WTN6_Line_1(Date[i]);	
 	}
 	
-	Delay_ms(500);			
-	WTN6_Line_1(Year);
-	Delay_ms(1000);
-//播报月	
-	if(Date[4] == 0x01)
+	Delay_ms(500);		
+	WTN6_Line_1(Year);				//播报年	
+	
+//播报月
+	switch(Date[4])
 	{
-		Delay_ms(1000);			
-		WTN6_Line_1(Date[4]);
-		
-		Delay_ms(1000);
-		WTN6_Line_1(Date[5]);
-		
-		Delay_ms(1000);	
-		WTN6_Line_1(Month);
-		Delay_ms(1000);
+		case 0x00:
+			Delay_ms(500);
+			WTN6_Line_1(Date[5]);		//月个位
+		break;
+		case 0x01:
+			Delay_ms(500);
+			WTN6_Line_1(WTN6_NUC_TE);	//播报十
+			if(Date[5] !=0x00)
+			{
+				Delay_ms(500);
+				WTN6_Line_1(Date[5]);		//月个位
+			}
+		break;
+		default:
+			Delay_ms(500);
+			WTN6_Line_1(Date[4]);	//播报月十位
+			Delay_ms(500);
+			WTN6_Line_1(WTN6_NUC_TE);	//播报十
+			Delay_ms(500);
+			WTN6_Line_1(Date[5]);		//月个位
 	}
+
+	Delay_ms(500);	
+	WTN6_Line_1(Month);				//播月
+	
+//播报日
+	switch(Date[6])
 	{
-		Delay_ms(1000);
-		WTN6_Line_1(Date[5]);
-		
-		Delay_ms(1000);	
-		WTN6_Line_1(Month);
-		Delay_ms(1000);
+		case 0x00:							//日第一位为0直接播报个位
+			Delay_ms(500);
+			WTN6_Line_1(Date[7]);	
+		break;
+		case 0x01:							//日第一位为1，播报十
+			Delay_ms(500);
+			WTN6_Line_1(WTN6_NUC_TE);		//播报十
+			if(Date[7] != 0x00)
+			{
+				Delay_ms(500);
+				WTN6_Line_1(Date[7]);			//播报日个位
+			}	
+		break;
+		default:							//日第一位不为0和1，、十、
+			Delay_ms(500);			
+			WTN6_Line_1(Date[6]);			//播报日十位
+			
+			Delay_ms(500);
+			WTN6_Line_1(WTN6_NUC_TE);		//播报十
+
+			Delay_ms(500);
+			WTN6_Line_1(Date[7]);			//播报日个位
 	}
-//播报日	
-	if(Date[6] == 0x00)
-	{
-		Delay_ms(1000);
-		WTN6_Line_1(Date[7]);
-		
-		Delay_ms(1000);	
-		WTN6_Line_1(Day);
-		Delay_ms(1000);
-	}
-	{
-		
-		Delay_ms(1000);			
-		WTN6_Line_1(Date[6]);
-		
-		Delay_ms(1000);
-		WTN6_Line_1(Date[7]);
-		
-		Delay_ms(1000);	
-		WTN6_Line_1(Day);
-		Delay_ms(1000);
-	}
+
+	Delay_ms(500);	
+	WTN6_Line_1(Day);
 }
 
 /*
  *@brief 		整点报时
- *				播报内容为“现在下午18点整”
+ *				播报内容为“现在是下午18点整”
  *@param[in]	Time：需要播报的时间。
  *				Time[0]=0x01    上午
  *				Time[0]=0x02 	下午
@@ -316,58 +393,74 @@ void WTN6_ReportDate(unsigned char Date[])
 void WTN6_ReportTime(unsigned char Time[])						
 {
 	WTN6_Time_Type OnHour,Mor,Aft,Nig,Now;
-	WTN6_Symbol_Type Dot;
+	WTN6_Symbol_Type Dot,Is;
+	WTN6_Num_Unit_CN_Type Ten;				    //十
 	
-	OnHour = WTN6_T_O;
-	Mor	   = WTN6_T_M;
-	Aft    = WTN6_T_A;
-	Nig    = WTN6_T_N;
+	OnHour = WTN6_T_OtH;
+	Mor	   = WTN6_T_Mor;
+	Aft    = WTN6_T_Aft;
+	Nig    = WTN6_T_Nig;
 	Now	   = WTN6_T_Now;
 	Dot	   = WTN6_S_D;
+	Is	   = WTN6_S_Is;
+	Ten	   = WTN6_NUC_TE;
 	
-	Delay_ms(1000);			
+	Delay_ms(500);			
 	WTN6_Line_1(Now);				//播报现在
-	Delay_ms(1000);
+	Delay_ms(500);
+	
+	Delay_ms(500);
+	WTN6_Line_1(Is);				//播报是
 	
 	switch(Time[0])
 	{
-		case 0x01:
-			Delay_ms(500);			
+		case 0x01:			
 			WTN6_Line_1(Mor);		//播报上午
 			Delay_ms(500);
 		break;
-		case 0x02:
-			Delay_ms(500);			
+		case 0x02:			
 			WTN6_Line_1(Aft);		//播报下午
 			Delay_ms(500);
 		break;
-		case 0x03:
-			Delay_ms(500);			
+		case 0x03:			
 			WTN6_Line_1(Nig);		//播报晚上
 			Delay_ms(500);
 		break;	
 	}
 //播报时间
-	if(Time[1] == 0x00)
+	switch(Time[1])
 	{
-		Delay_ms(1000);			
-		WTN6_Line_1(Time[2]);			
-	}else
-	{
-		Delay_ms(1000);			
-		WTN6_Line_1(Time[1]);
-		
-		Delay_ms(1000);			
-		WTN6_Line_1(Time[2]);	
+		case 0x00:					//时间十位为0，只播个位
+			Delay_ms(500);			
+			WTN6_Line_1(Time[2]);
+		break;
+		case 0x01:					//时间十位为1，播十
+			Delay_ms(500);			
+			WTN6_Line_1(Ten);
+			if(Time[2]!=0x00)
+			{
+				Delay_ms(500);			
+				WTN6_Line_1(Time[2]);
+			}			
+		break;
+		default:					//时间十位不为1、0
+			Delay_ms(500);			
+			WTN6_Line_1(Time[1]);	//播时间十位	
+			
+			Delay_ms(500);			
+			WTN6_Line_1(Ten);		//播十
+			
+			Delay_ms(500);			
+			WTN6_Line_1(Time[2]);	//播时间个位
+
 	}
 	
-	Delay_ms(1000);
+	Delay_ms(500);
 	WTN6_Line_1(Dot);			//播报点
 	
-	Delay_ms(1000);
+	Delay_ms(500);
 	WTN6_Line_1(OnHour);			//播报整
-	Delay_ms(1000);
-
+	Delay_ms(500);
 }
 
 
--- WTN6/wtn6.h
+++ WTN6/wtn6.h
@@ -7,20 +7,20 @@
 sbit	WNT6_DATA	=	P5^0;			//WTN6数据IO
 
 /*								WTN6唯创知音语音内容地址对应表-2020年8月7日
-|   语音内容	|	地址 |	*	|   语音内容	|	地址 |	*	|   语音内容	|	地址 |	*	|   语音内容	|	地址 |
-|		1		|   0x00 |	*	|		8		|   0x08 |	*	|	  今天		|   0x10 |	*	|     晚上		|   0x18 |
-|		1		|   0x01 |	*	|		9		|   0x09 |	*	|	   年		|   0x11 |	*	|     现在		|   0x19 |
-|		2		|   0x02 |	*	|		*		|   0x0A |	*	|	   月		|   0x12 |	*	|      度		|   0x1A |
-|		3		|   0x03 |	*	|		#		|   0x0B |	*	|	   日		|   0x13 |	*	|    百分之		|   0x1B |
-|		4		|   0x04 |	*	|	  来电		|   0x0C |	*	|	   点		|   0x14 |	*	|				|   	 |
-|		5		|   0x05 |	*	| “电话铃铃声”|   0x0D |	*	|	   整		|   0x15 |	*	|				|		 |
-|		6		|   0x06 |	*	|	  温度		|   0x0E |	*	|	  上午		|   0x16 |	*	|				|        |
-|		7		|   0x07 |	*	|	  湿度		|   0x0F |	*	|	  下午		|   0x17 |	*	|				|        |			
+|   语音内容	|	地址 |	*	|   语音内容	|	地址 |	*	|   语音内容	|	地址 |	*	|   语音内容	|	地址 |	*	|   语音内容	|	地址 |
+|		1		|   0x00 |	*	|		8		|   0x08 |	*	|	  今天		|   0x10 |	*	|      整		|   0x18 |	*	|      是		|	0x20 |
+|		1		|   0x01 |	*	|		9		|   0x09 |	*	|	   年		|   0x11 |	*	|     上午		|   0x19 |	*	|      个		|	0x21 |
+|		2		|   0x02 |	*	|		*		|   0x0A |	*	|	   月		|   0x12 |	*	|     下午		|   0x1A |	*	|      时		|	0x22 |
+|		3		|   0x03 |	*	|		#		|   0x0B |	*	|	   日		|   0x13 |	*	|     晚上		|   0x1B |	*	|      白		|	0x23 |
+|		4		|   0x04 |	*	|	  来电		|   0x0C |	*	|	   时		|   0x14 |	*	|	  现在		|   0x1C |	*	|      前		|	0x24 |
+|		5		|   0x05 |	*	| “电话铃铃声”|   0x0D |	*	|	   分		|   0x15 |	*	|	  度（°）	|	0x1D |	*	|      万		|	0x25 |
+|		6		|   0x06 |	*	|	  温度		|   0x0E |	*	|	   秒		|   0x16 |	*	|	  百分之	|   0x1E |
+|		7		|   0x07 |	*	|	  湿度		|   0x0F |	*	|	   点		|   0x17 |	*	|	  摄氏度	|   0x1F |			
 */
 //数字0-9
 typedef enum WTN6_Number
 {
-	WTN6_N_0 = 0x00,
+	WTN6_N_0    = 0x00,
 	WTN6_N_1,
 	WTN6_N_2,
 	WTN6_N_3,
@@ -35,18 +35,20 @@ typedef enum WTN6_Number
 //符号类
 typedef enum WTN6_Symbol
 {
-	WTN6_S_A  = 0x0A,					// *
-	WTN6_S_N  = 0x0B,					// #
-	WTN6_S_D  = 0x14,					//点
-	WTN6_S_TC = 0x1A,					// °Celsius
-	WTN6_S_TH = 0x1B,					//%	
+	WTN6_S_A    = 0x0A,					// *
+	WTN6_S_N    = 0x0B,					// #
+	WTN6_S_D    = 0x17,					//点
+	WTN6_S_Deg  = 0x1D,					// °（度）
+	WTN6_S_TH   = 0x1E,					//%	（百分之）
+	WTN6_S_TC   = 0x1F,					//Celsius(摄氏度）
+	WTN6_S_Is	= 0x20,				    //是
 }WTN6_Symbol_Type;
 
 //来电、电话铃声（音乐）
 typedef enum WTN6_Phone
 {
-	WTN6_P_IC  = 0x0C,					//来电Incoming Call
-	WTN6_P_ICM = 0x0D,					//电话铃声（音乐）
+	WTN6_P_IC   = 0x0C,					//来电Incoming Call
+	WTN6_P_ICM  = 0x0D,					//电话铃声（音乐）
 }WTN6_Phone_Type;
 
 //温度、湿度
@@ -68,13 +70,25 @@ typedef enum WTN6_Date
 //时间
 typedef enum WTN6_Time
 {
-	WTN6_T_O	  = 0x15,				//整
-	WTN6_T_M 	  = 0x16,				//上午
-	WTN6_T_A      = 0x17,				//下午
-	WTN6_T_N      = 0x18,				//晚上
-	WTN6_T_Now    = 0x19,				//现在
+	WTN6_T_Hour	= 0x14,				//时
+	WTN6_T_Min	= 0x15,				//分
+	WTN6_T_Sec	= 0x16,				//秒	
+	WTN6_T_OtH	= 0x18,				//整
+	WTN6_T_Mor 	= 0x19,				//上午
+	WTN6_T_Aft  = 0x1A,				//下午
+	WTN6_T_Nig  = 0x1B,				//晚上
+	WTN6_T_Now  = 0x1C,				//现在
 }WTN6_Time_Type;
 
+
+typedef enum WTN6_Num_Unit_CN
+{
+	WTN6_NUC_I	= 0x21, 				//个
+	WTN6_NUC_TE	= 0x22, 				//十
+	WTN6_NUC_H	= 0x23, 				//百
+	WTN6_NUC_TH	= 0x24, 				//千
+	WTN6_NUC_M	= 0x25, 				//万
+}WTN6_Num_Unit_CN_Type;
 
 /*
  *@brief 		微秒延时函数			
@@ -108,7 +122,7 @@ extern void WTN6_Num(unsigned char Num[],int Len);
  *@param[in]	Num[]：需要播报的数字，最多3位，小数点不用包含，
  *			    例如23.8度，为Num[]={0x02,0x03,0x08}
  */
-extern void WTN6_Temp(unsigned char Num[]);
+extern void WTN6_Temp(unsigned char Temp[]);
 
 /*
  *@brief 		播报湿度
@@ -116,7 +130,7 @@ extern void WTN6_Temp(unsigned char Num[]);
  *@param[in]	Num[]：需要播报的数字，最多3位，小数点不用包含，
  *			    例如60.8%，为Num[]={0x06,0x00,0x08}
  */
-extern void WTN6_Hump(unsigned char Num[]);
+extern void WTN6_Hump(unsigned char Hump[]);
 
 /*
  *@brief 		来电铃声，响铃声（音乐形式）			
